fcf5f47734c5978f5afe33ad52bc4d8033899b34
ayala <deus@ubuntu.ubuntu-domain>
2018-04-10 14:46:28 -0700

    add genPraductData

--- datagen.js
+++ datagen.js
@@ -1,6 +1,27 @@
 const faker = require('faker');
 const _ = require('lowdash');
+const { bulkInsertProducts } = require('./database');
 
-const generateProductData = () => {
+const generateProductData = (counter) => {
+  let requests = [];
+
+  for(let i = 0; i < 24; i++) {
+    const request = {
+      putRequest: {
+        Item: {
+          productId: counter,
+          name: faker.commerce.productName(),
+          quantity: (Math.random() * 4500).toFixed(2),
+        }
+      }
+    }
+    counter += 1;
+    requests.push(request);
+  }
+  bulkInsertProducts(requests, (response) => {
+    if(counter < 10000000) {
+      generateProductData(counter);
+    }
+  });
 
 };
